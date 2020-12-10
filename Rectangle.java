fc6e9df2f0cb86bd5a79975f5ce301f911ba3891
MEK77 <72091074+MEK77@users.noreply.github.com>
2020-12-10 16:25:00 +0200

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,46 @@
+package Trying;
+
+public class Rectangle {
+
+    private double length;
+
+    private double width;
+
+    public void setLength(double shit1)
+    {
+
+        length = shit1 ;
+
+    }
+
+    public void setWidth(double shit2)
+    {
+
+        width = shit2 ;
+
+    }
+
+    public double getLength()
+    {
+
+     return length ;
+
+    }
+
+    public double getwidth()
+    {
+
+        return width ;
+
+    }
+
+    public double getarea()
+    {
+
+        return length * width ;
+
+    }
+
+
+
+}
